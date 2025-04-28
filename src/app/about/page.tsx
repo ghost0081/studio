13c92b691ad1a4a7e9ba62ddc908af56607927eb
praneet Singh <praneetsingh081105@gmail.com>
2025-04-28 12:22:00 +0000

    new

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,13 +3,7 @@ export default function About() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          About Us
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          This is the About page. Learn more about our dance school.
-        </p>
+       
       </main>
     </div>
   );
