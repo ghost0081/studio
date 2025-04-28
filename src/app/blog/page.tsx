a27e815c1432750f37c95867e6bf2ef080db0bb4
praneet Singh <praneetsingh081105@gmail.com>
2025-04-28 11:56:48 +0000

    remove blog and read our dance blog from blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,13 +3,6 @@ export default function Blog() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Blog
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Read our dance blog.
-        </p>
       </main>
     </div>
   );
