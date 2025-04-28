e5d75c822e094be4c4c526a834e3cd630f600b5c
praneet Singh <praneetsingh081105@gmail.com>
2025-04-28 11:57:39 +0000

    create a blog heading page option by which clicking on it will redirect to that content page

---
+++ src/app/blog/blog-1/page.tsx
@@ -0,0 +1,16 @@
+
+export default function Blog1() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-4xl font-bold">
+          Blog Post 1 Content
+        </h1>
+
+        <p className="mt-3 text-2xl">
+          This is the content of blog post 1.
+        </p>
+      </main>
+    </div>
+  );
+}
---
+++ src/app/blog/blog-2/page.tsx
@@ -0,0 +1,16 @@
+
+export default function Blog2() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-4xl font-bold">
+          Blog Post 2 Content
+        </h1>
+
+        <p className="mt-3 text-2xl">
+          This is the content of blog post 2.
+        </p>
+      </main>
+    </div>
+  );
+}
