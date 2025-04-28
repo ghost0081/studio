575132fde38b491c2d4633a7d759ff6c48077b0a
praneet Singh <praneetsingh081105@gmail.com>
2025-04-28 12:06:56 +0000

    by clicking a blog it will redirect to that blog page information

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -54,8 +54,7 @@ function BlogCard({
   );
 }
 
-
-export default function Blog() {
+export default function BlogPage() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Blog</h1>
